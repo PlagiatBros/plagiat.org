6d2005f18a80622c0c8ac25c45eb15267dbfce13
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2018-04-24 08:59:32 +0200

    some lil tings

--- src/lang/local_fr.php
+++ src/lang/local_fr.php
@@ -1,4 +1,9 @@
 <?php
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // A
+    'agenda_noisegate' => "J'en ai rien à foutre de vos conneries, je veux juste voir les conssscerts",
+    'agenda_oldgate' => "Je suis un passsséiste, je veux tout voir",
+
 );
 ?>
